498ddbdd913252d0163f048d0534f8ccac996bae
Mark Chandler <markchandlercpt@gmail.com>
2026-06-18 14:36:25 +0200

    updating repo to retrigger the build

--- Tests/PluginTests.cpp
+++ Tests/PluginTests.cpp
@@ -15,3 +15,4 @@ TEST_CASE ("RumbleRoom exposes expected parameters", "[processor]")
     REQUIRE (processor.apvts.getParameter ("hpCutoff") != nullptr);
     REQUIRE (processor.apvts.getParameter ("cutoff") != nullptr);
 } 
+ 
