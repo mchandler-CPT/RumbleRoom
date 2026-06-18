485ac0f88733b20993b0b9d42f9d8bbb2fa29519
Mark Chandler <markchandlercpt@gmail.com>
2026-06-18 14:25:10 +0200

    just kicking off the build again

--- Tests/PluginTests.cpp
+++ Tests/PluginTests.cpp
@@ -14,4 +14,4 @@ TEST_CASE ("RumbleRoom exposes expected parameters", "[processor]")
     REQUIRE (processor.apvts.getParameter ("delayTime") != nullptr);
     REQUIRE (processor.apvts.getParameter ("hpCutoff") != nullptr);
     REQUIRE (processor.apvts.getParameter ("cutoff") != nullptr);
-}
+} 
